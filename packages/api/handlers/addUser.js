0af308d9dacb75f545fee937ea7851539e7c44eb
bhushan.kumar <=>
2023-02-28 10:06:18 +0530

    database_migration

--- packages/api/handlers/addUser.js
+++ packages/api/handlers/addUser.js
@@ -1,13 +1,16 @@
 const { addUser } = require("../common_layer/services/user");
-const { res_201 } = require("../common_layer/util/CustomResponse");
+const { res_201, res_400 } = require("../common_layer/util/CustomResponse");
 
 module.exports.handler = async (event) => {
-    const body = event.body;
-    try{
+  const body = JSON.parse(event.body);
+  try {
+    if (typeof body?.firstName == "string" && typeof body?.lastName == "string" && typeof body?.email == "string") {
       const user = await addUser(body);
-      return res_201({user});
-    }catch(e){
-      console.log(e);
+      return res_201({ user });
+    } else {
+      return res_400({ message: "Invalid Data!" })
     }
-  };
-  
+  } catch (e) {
+    console.log(e);
+  }
+};
