c9bf404a52b6f32ab2048dcbbbb908a67f54e74f
bhushan.kumar <=>
2023-03-01 13:21:19 +0530

    fixes

--- packages/webapp/src/pages/User/User.js
+++ packages/webapp/src/pages/User/User.js
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
 import axios from "axios";
+import AppBar from '@mui/material/AppBar';
+import Typography from '@mui/material/Typography';
+import Toolbar from '@mui/material/Toolbar';
+import Box from "@mui/material/Box";
 
 const User = () => {
   const [user, setUser] = useState({
@@ -22,6 +26,42 @@ const User = () => {
   };
   return (
     <div className="container py-4">
+      <AppBar
+        position="absolute"
+        color="default"
+        elevation={0}
+        sx={{
+          position: 'relative',
+          borderBottom: (t) => `1px solid ${t.palette.divider}`,
+        }}
+      >
+        <Toolbar style={{width: "100%",justifyContent: 'center'}} align="center">
+          <Typography style={{width:"100%"}} align="center"  variant="h6" color="inherit" noWrap>
+            POC-GROUP-1
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Box
+        sx={{
+          mx: "auto",
+          width: 1000,
+          p: 1,
+          // m: 1,
+          mt: 5,
+          ml: 17,
+          bgcolor: (theme) =>
+            theme.palette.mode === "dark" ? "#101010" : "grey.50",
+          color: (theme) =>
+            theme.palette.mode === "dark" ? "grey.300" : "grey.800",
+          border: "1px solid",
+          borderColor: (theme) =>
+            theme.palette.mode === "dark" ? "grey.800" : "grey.300",
+          borderRadius: 2,
+          textAlign: "center",
+          fontSize: "0.875rem",
+          fontWeight: "700",
+        }}
+      >
       <Link className="btn btn-primary" to="/landing-page">
         back to Home
       </Link>
@@ -34,6 +74,7 @@ const User = () => {
         <li className="list-group-item">phone: {user.phone}</li>
         <li className="list-group-item">website: {user.website}</li>
       </ul>
+      </Box>
     </div>
   );
 };
