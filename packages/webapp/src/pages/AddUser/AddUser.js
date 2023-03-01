6e831f8f94a94f782b151f192ed18e82c774385b
bhushan.kumar <=>
2023-03-01 15:11:58 +0530

    fix

--- packages/webapp/src/pages/AddUser/AddUser.js
+++ packages/webapp/src/pages/AddUser/AddUser.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate, useParams } from "react-router-dom";
 import Button from "@mui/material/Button";
